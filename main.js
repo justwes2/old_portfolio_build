7e4c35e0ac3946816e8f4a8686472faa05a023de
Wes <wes.coffay@gmail.com>
2017-01-31 16:23:30 -0500

    added link for Towers project

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-console.log("JS file is connected to HTML! Woo!")
-// document.getElementsByClass('project').addEventListener('click', alert('Stay tuned to see me grow as a developer!'))
-
-var project = document.getElementsByClassName("project");
-
-var myFunction = function() {
-    var attribute = this.getAttribute("data-myattribute");
-    alert('Stay tuned to see me grow as a developer!');
-};
-
-for (var i = 0; i < project.length; i++) {
-    project[i].addEventListener('click', myFunction, false);
-}
+// console.log("JS file is connected to HTML! Woo!")
+// // document.getElementsByClass('project').addEventListener('click', alert('Stay tuned to see me grow as a developer!'))
+//
+// var project = document.getElementsByClassName("project");
+//
+// var myFunction = function() {
+//     var attribute = this.getAttribute("data-myattribute");
+//     alert('Stay tuned to see me grow as a developer!');
+// };
+//
+// for (var i = 0; i < project.length; i++) {
+//     project[i].addEventListener('click', myFunction, false);
+// }
